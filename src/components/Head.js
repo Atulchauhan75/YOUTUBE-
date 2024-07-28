54d35bbe4b3b8dff2d1158838188d354efa5e261
Atulchauhan75 <atulchauhan1903@gmail.com>
2024-07-28 19:26:21 +0530

    enter functionality on search bar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/constants";
 import { cacheResults } from "../utils/searchSlice";
-import UserSearch from "./UserSearch";
-import {
-  Link,
-  NavLink,
-  Router,
-  useNavigate,
-  useRoutes,
-} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { setSearchValue } from "../utils/userSearchSlice";
+
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
@@ -19,11 +13,12 @@ const Head = () => {
   const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
-  
-  const getSearchSuggestions = async () => {
+
+  const getSearchSuggestions = useCallback(async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
@@ -32,39 +27,41 @@ const Head = () => {
         [searchQuery]: json[1],
       })
     );
-  };
+  }, [searchQuery, dispatch]);
+
   const handleSuggestionClick = (s) => {
     setSearchQuery(s);
-    setShowSuggestions(false);
-    // <UserSearch query={searchQuery}/>
-  };
-  const handleSearchClick = () => {
-    // alert("hf jh jh hj bj bj b");
-    console.log(searchQuery);
     dispatch(setSearchValue(searchQuery));
     navigate("/results");
     setShowSuggestions(false);
   };
-  
-    //This is DEBOUNCING
-    useEffect(() => {
-      //make API Call here
-      const timer = setTimeout(() => {
-        if (searchCache[searchQuery]) {
-          setSuggestions(searchCache[searchQuery]);
-        } else {
-          getSearchSuggestions();
-        }
-      }, 200);
-      return () => {
-        clearTimeout(timer);
-      };
-    }, [searchQuery]);
+
+  const handleSearchClick = () => {
+    // console.log(searchQuery);
+    dispatch(setSearchValue(searchQuery));
+    navigate("/results");
+    setShowSuggestions(false);
+  };
+
+  useEffect(() => {
+    // Debouncing
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery, searchCache, getSearchSuggestions]);
+
   return (
-    <div className="fixed top-0 left-0 bg-white m-0  w-full block z-10 ">
-      <div className="  grid grid-flow-col  p-3 shadow-lg  ">
-        {/*First section of head*/}
-        <div className="flex col-span-1 m-2 ">
+    <div className="fixed top-0 left-0 bg-white m-0 w-full block z-10 ">
+      <div className="grid grid-flow-col p-3">
+        {/* First section of head */}
+        <div className="flex col-span-1 m-2">
           <img
             onClick={toggleMenuHandler}
             src="https://static.vecteezy.com/system/resources/previews/002/292/406/large_2x/hamburger-menu-line-icon-free-vector.jpg"
@@ -74,38 +71,43 @@ const Head = () => {
           <img
             src="https://tse1.mm.bing.net/th?id=OIP.sCtdNjphAin-gugu0MNptAHaEK&pid=Api&P=0&h=180"
             alt="Youtube Logo"
-            className="h-12 mx-2 "
+            className="h-12 mx-2"
             onClick={() => console.log("hello")}
           />
         </div>
 
-        {/* {2nd section of head} */}
-        <div className="col-span-10 ">
+        {/* Second section of head */}
+        <div className="col-span-10">
           <div>
             <input
-              className=" mt-2 ml-52 w-[36rem] rounded-l-full px-5 border border-gray-500 p-2 shadow-sm rounded-md "
+              className="mt-2 ml-52 w-[36rem] rounded-l-full px-5 border border-gray-500 p-2 shadow-sm rounded-md"
               type="text"
               placeholder="Search"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e?.target?.value)}
               onFocus={() => setShowSuggestions(true)}
-              //onBlur means FOCUS OUT
+              // onBlur means FOCUS OUT
               // onBlur={() => setShowSuggestions(false)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearchClick();
+                }
+              }}
             />
             <button
-              className="border border-gray-500  px-5 p-2 bg-gray-200 rounded-r-full"
+              className="border border-gray-500 px-5 p-2 bg-gray-200 rounded-r-full"
               onClick={() => handleSearchClick()}
             >
               🔍
             </button>
-            <div className=" ml-52 bg-white fixed  w-[36rem] border border-gray-200 rounded-lg">
+            <div className="ml-52 bg-white fixed w-[36rem] border border-gray-200 rounded-lg">
               <ul>
                 {showSuggestions &&
                   suggestions.map((s, index) => (
                     <li
                       onClick={() => handleSuggestionClick(s)}
                       key={index}
-                      className="py-2 px-3 shadow-sm hover:bg-gray-200 rounded-sm w-full "
+                      className="py-2 px-3 shadow-sm hover:bg-gray-200 rounded-sm w-full"
                     >
                       🔍 {s}
                     </li>
@@ -115,7 +117,7 @@ const Head = () => {
           </div>
         </div>
 
-        {/* {3rd section of head} */}
+        {/* Third section of head */}
         <div className="col-span-1">
           <img
             src="https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png"
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect,  } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
@@ -11,10 +11,10 @@ const VideoContainer = () => {
   const nextToken = useSelector((store) => store.videoStore.nextToken);
   const bottomReached = useSelector((store) => store.videoStore.setBottom);
 
-  const getVideos = async () => {
+  const getVideos = useCallback(async () => {
     try {
       // console.log("Fetching initial videos");
-      
+
       // Check if videos are already present
       if (videos.length === 0) {
         const data = await fetch(YOUTUBE_VIDEOS_API);
@@ -22,7 +22,6 @@ const VideoContainer = () => {
         // console.log("Initial videos fetched:", json.items.length);
 
         dispatch(setNextToken(json?.nextPageToken));
-
         dispatch(addVideos(json?.items));
       } 
       // else {
@@ -31,29 +30,28 @@ const VideoContainer = () => {
     } catch (error) {
       console.log(error);
     }
-  };
+  }, [dispatch, videos.length]); // Include dispatch and videos.length as dependencies
+
   useEffect(() => {
     getVideos();
-  }, []);
-  const nextVideos = async () => {
+  }, [getVideos]);
+  const nextVideos = useCallback(async () => {
     if (nextToken) {
       const url = `${YOUTUBE_VIDEOS_API}&pageToken=${nextToken}`;
 
       const data = await fetch(url);
       const json = await data.json();
-      // console.log(json);
       dispatch(setNextToken(json?.nextPageToken));
       dispatch(addVideos(json?.items));
       dispatch(setBottom(false));
     }
-  };
+  }, [dispatch, nextToken]);
 
   useEffect(() => {
     if (bottomReached) {
-      // console.log("inside the bottom useEffect ......");
       nextVideos();
     }
-  }, [bottomReached]);
+  }, [bottomReached, nextVideos]);
 
   // Handle the case where videos is not an array
   return (
